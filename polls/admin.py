c7052d83e60d727ab739eaa6bec7609e67515c64
unfriend <t-moore@mail.ru>
2015-11-16 23:38:25 +0300

    initial commit (starts from django tutorial app)

---
+++ polls/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import Choice, Question
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Question',               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
