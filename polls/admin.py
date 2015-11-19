03111139121ec87f6899bc1d7b1ceb742eae163b
unfriend <t-moore@mail.ru>
2015-11-19 22:56:15 +0300

    + polls index and detail pages

--- polls/admin.py
+++ polls/admin.py
@@ -2,11 +2,20 @@ from django.contrib import admin
 
 from .models import Choice, Question
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        ('Question',               {'fields': ['question_text']}),
+        (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
