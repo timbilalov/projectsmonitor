d705bdb41f20df67d34b53faca5d42ba9dfd5d19
unfriend <t-moore@mail.ru>
2016-03-15 08:49:14 +0300

    * detailed view (begin to collect data)

--- sites/views.py
+++ sites/views.py
@@ -34,25 +34,10 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Site
     template_name = 'sites/detail.html'
-    context_object_name = 'site'
-
-    # sites = [
-    #     "http://bitell.itsoft.su/",
-    #     "http://venarus.itsft.ru/",
-    #     "http://bitell.ru/",
-    #     "http://amurbosch.com/",
-    #     "http://new.bss-bigtool.ru/",
-    #     "http://forus.itsft.ru/",
-    #     "http://itsoft.ru/"
-    # ]
-
-
-    # def get():
-    #     pass
+    context_object_name = 'report_data'
 
 
     def get_object(self):
-
 
         def hostname_resolves(hostname):
             try:
@@ -60,32 +45,34 @@ class DetailView(generic.DetailView):
                 return 1
             except socket.error:
                 return 0
+
         # https://docs.python.org/2/library/re.html
         # https://regex101.com/r/oQ2jS7/1
 
         report_data = {}
         sites = Site.objects.all()
+        # return site.object
+        # for s in site:
+        #     report_data[s] = site[s]
 
-        for site in sites:
-            s = site.dev_url
-            if report_data.get("_ all sites") == None:
-                report_data["_ all sites"] = 1
-            else:
-                report_data["_ all sites"] += 1
+        # return self.pk_url_kwarg
+        site = super(DetailView, self).get_object()
+        # return site
+        report_data["site_object"] = site
 
-            # print "\n\n" + s
+        s = site.dev_url
+        urls_to_check = {
+            "dev": site.dev_url,
+            "prod": site.prod_url
+        }
+
+        for env in urls_to_check:
+            s = urls_to_check[env]
 
             url = urlparse(s)
-            # print url
+            rep_data_env = {}
             if not hostname_resolves(url.netloc):
-                # print "not resolves"
-
-                if report_data.get("not resolves") == None:
-                    report_data["not resolves"] = 1
-                else:
-                    report_data["not resolves"] += 1
-
-                continue
+                rep_data_env["not_resolves"] = True
 
             buffer = BytesIO()
             c = pycurl.Curl()
@@ -93,58 +80,29 @@ class DetailView(generic.DetailView):
             c.setopt(c.WRITEDATA, buffer)
             c.perform()
 
-            # HTTP response code, e.g. 200.
             resp_code = c.getinfo(c.RESPONSE_CODE)
+            rep_data_env["response_code"] = resp_code
+
             if resp_code == 200:
-                # print s + "robots.txt"
                 buffer = BytesIO()
                 c.setopt(c.URL, s + "robots.txt")
                 c.setopt(c.WRITEDATA, buffer)
                 c.perform()
-                if c.getinfo(c.RESPONSE_CODE) == 200:
+                resp_code_2 = c.getinfo(c.RESPONSE_CODE)
+
+                rep_data_env["robots_txt_response_code"] = resp_code_2
+                if resp_code_2 == 200:
                     robots = buffer.getvalue()
-                    # print(robots)
-                    # p = re.compile("(?<!\#\s|.)disallow:\s*\/(?![\w\R\?\*])", re.IGNORECASE)
                     p = re.compile("(?<!\#)(?<!.)Disallow:\s*\/(?![\w\R\?\*])", re.IGNORECASE)
-                    # m = p.search(robots)
-                    # m.group(0)
-                    if len(p.findall(robots)) > 0:
-                        # print "Found 'Disallow: /' string in robots.txt. Site closed for one or all robots."
+                    rep_data_env["found_disallow"] = len(p.findall(robots)) > 0
+                    rep_data_env["robots_txt_value"] = robots
+                # c.close()
 
-                        if report_data.get("disallow") == None:
-                            report_data["disallow"] = 1
-                        else:
-                            report_data["disallow"] += 1
-                    else:
-                        # print "Site opened for all robots."
-
-                        if report_data.get("allow") == None:
-                            report_data["allow"] = 1
-                        else:
-                            report_data["allow"] += 1
-
-                    c.close()
-                # else:
-                #     print s + "robots.txt response code: " + str(c.getinfo(c.RESPONSE_CODE))
-
-            # else:
-            #     print('Status: %d' % resp_code)
-
-            if report_data.get(str(resp_code)) == None:
-                report_data[str(resp_code)] = 1
-            else:
-                report_data[str(resp_code)] += 1
-
-
-            # Elapsed time for the transfer.
-            # print('Status: %f' % c.getinfo(c.TOTAL_TIME))
-
-            # getinfo must be called before close.
             c.close()
 
-        # print report_data
+            report_data[env] = rep_data_env
+
         return report_data
-        # return sites
 
     # def get_queryset(self):
     #     """
